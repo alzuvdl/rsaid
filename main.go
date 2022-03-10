df35058abf86d64b0f9fefd1f0bde555e203bc82
Jaco van der Loo <jaco.vanderloo@gmail.com>
2022-03-10 15:30:46 +0400

    update comments

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type Details struct {
 
 var errorMessage = "invalid south african id number"
 
-// Determines if the given ID number is valid using the Luhn algorithm.
+// IsValid determines if the given ID number is valid using the Luhn algorithm.
 func IsValid(id string) bool {
 	var sum int
 	var alternate bool
@@ -37,15 +37,14 @@ func IsValid(id string) bool {
 	return sum%10 == 0
 }
 
-// Determines the gender of the person from the ID number.
-// This is calculated by using the 7th digit in the 13 digit ID number.
-// A 7th digit value between zero and four is considered female, five to nine is considered male.
+// Gender determines if the person is male or female.
+// Gender is calculated by using the 7th digit in the 13 digit ID number.
+// Zero to four is considered female, five to nine is considered male.
 // It returns either "male" or "female" and any errors encountered.
 func Gender(id string) (string, error) {
 	if !IsValid(id) {
 		return "", errors.New(errorMessage)
 	}
-	// Don't need to check for error here.. the ID string has already been validated?
 	gender, _ := strconv.Atoi(id[6:7])
 	if gender < 5 {
 		return "female", nil
@@ -53,9 +52,9 @@ func Gender(id string) (string, error) {
 	return "male", nil
 }
 
-// Determines South African citizenship of the person from the ID number.
-// This is calculated by using the 11th digit in the 13 digit ID number.
-// An 11th digit value of zero is considered a citizen, otherwise, it is considered a permanent resident.
+// IsCitizen determines if the person is a South African citizen.
+// Citizenship is calculated by using the 11th digit in the 13 digit ID number.
+// Zero is considered a citizen, otherwise, it is considered a permanent resident.
 // It returns true if the person is a citizen and any errors encountered.
 func IsCitizen(id string) (bool, error) {
 	if !IsValid(id) {
@@ -65,8 +64,8 @@ func IsCitizen(id string) (bool, error) {
 	return citizenCode == "0", nil
 }
 
-// Determines the date of birth of the person from the ID number.
-// This is calculated by using the first 6 digits in the 13 digit ID number.
+// DateOfBirth calculates the date of birth of the person.
+// Date of birth is calculated by using the first 6 digits in the 13 digit ID number.
 // The first pair of digits are the year, the second pair is the month and the third pair is the day.
 // It returns the date of birth and any errors encountered.
 func DateOfBirth(id string) (time.Time, error) {
@@ -101,7 +100,7 @@ func DateOfBirth(id string) (time.Time, error) {
 	return dob, nil
 }
 
-// Parses a South African ID number to derive details from it.
+// Parse derives details from a South African ID number.
 // Details include gender, citizenship, and date of birth.
 // It returns the details and any errors encountered.
 func Parse(id string) (Details, error) {
